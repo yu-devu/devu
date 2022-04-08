defcf22b6cfa8e5eff853101e14a88c51ad1a77f
hcs4125 <hcs4125@gmail.com>
2022-04-08 20:35:24 +0900

    update::Transactional and logs

--- backend/src/main/java/com/devu/backend/entity/BaseTime.java
+++ backend/src/main/java/com/devu/backend/entity/BaseTime.java
@@ -1,7 +1,5 @@
 package com.devu.backend.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
