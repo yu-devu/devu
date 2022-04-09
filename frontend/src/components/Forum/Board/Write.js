97b197da2b157ef89359b583cbc00a6a269c4dc6
“깃 <hy04730@naver.com>
2022-04-09 19:55:09 +0900

    feat: 게시글 작성

--- frontend/src/components/Forum/Board/Write.js
+++ frontend/src/components/Forum/Board/Write.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './write.css';
 
 const Write = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleTitle = (e) => setTitle(e.target.value);
+  const handleContent = (e) => setContent(e.target.value);
+  const handleWrite = () => {
+    if (title === '' || content === '') {
+      alert('글을 작성해주세요!');
+      return
+    }
+    console.log("title: ", title);
+    console.log("content: ", content);
+    alert('글이 성공적으로 등록되었습니다!');
+    navigate(-1);
+  }
+
   return (
     <div className="container-write">
       <div className="write-area">
@@ -15,6 +33,7 @@ const Write = () => {
             placeholder="제목"
             maxlength="100"
             required
+            onChange={(e) => handleTitle(e)}
           ></textarea>
         </div>
         <div className="in_content">
@@ -25,15 +44,16 @@ const Write = () => {
             cols="55"
             placeholder="내용"
             required
+            onChange={(e) => handleContent(e)}
           ></textarea>
         </div>
         <div className="bt_se">
-          <button className="btn-post" type="submit">
+          <button className="btn-post" onClick={() => handleWrite()}>
             글 작성
           </button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
