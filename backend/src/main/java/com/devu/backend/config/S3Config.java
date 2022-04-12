eaad956f2910e7dd6b2ce6b80701b56dbdb31613
hcs4125 <hcs4125@gmail.com>
2022-04-12 22:11:09 +0900

    update: yml to properties

--- backend/src/main/java/com/devu/backend/config/S3Config.java
+++ backend/src/main/java/com/devu/backend/config/S3Config.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:config/aws.yml")
+@PropertySource("classpath:config/aws.properties")
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
