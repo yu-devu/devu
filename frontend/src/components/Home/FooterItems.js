9f46fec1d0c55ef4df72e9cd176932f4d55a3650
jy05un <jys000305@naver.com>
2022-04-23 02:29:49 +0900

    main page css sample v1.0

---
+++ frontend/src/components/Home/FooterItems.js
@@ -0,0 +1,20 @@
+export const footerItems = [
+    {
+        id: 1,
+        title: "개인정보취급방침",
+        path: "./",
+        cName: "footer-item",
+    },
+    {
+        id: 2,
+        title: "이용약관",
+        path: "./",
+        cName: "footer-item",
+    },
+    {
+        id: 3,
+        title: "고객센터",
+        path: "./",
+        cName: "footer-item",
+    }
+];
