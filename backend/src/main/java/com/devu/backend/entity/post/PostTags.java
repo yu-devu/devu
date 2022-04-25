ac4dd5c5a4b34a3cc914fe6e24965bcbd4dbb494
hcs4125 <hcs4125@gmail.com>
2022-04-25 22:53:47 +0900

    update:Tags Enum

---
+++ backend/src/main/java/com/devu/backend/entity/post/PostTags.java
@@ -0,0 +1,21 @@
+package com.devu.backend.entity.post;
+
+public enum PostTags {
+    SPRING,
+    C,
+    CPP,
+    CSHARP,
+    JAVASCRIPT,
+    REACT,
+    VUE,
+    NODE,
+    PYTHON,
+    DJANGO,
+    FLASK,
+    GO,
+    SWIFT,
+    ANGULAR,
+    RUBY,
+    JAVA,
+    FLUTTER
+}
