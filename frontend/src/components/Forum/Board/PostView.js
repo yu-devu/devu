2742118dffee4c8b810cb7f3cbc407fd3ef88383
hcs4125 <hcs4125@gmail.com>
2022-04-30 21:15:39 +0900

    fix:html-react-parser

--- frontend/src/components/Forum/Board/PostView.js
+++ frontend/src/components/Forum/Board/PostView.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useLocation, Link } from 'react-router-dom';
-import ReactHtmlParser from "react-html-parser";
-import "./postView.css"
+import { useLocation, Link } from "react-router-dom";
+import "./postView.css";
 
 const PostView = () => {
   const location = useLocation();
   const [postData, setPostData] = useState([]);
   let pathname = location.pathname;
-  let [a, b, postId] = pathname.split('/');
+  let [a, b, postId] = pathname.split("/");
   // useLocation으로 pathname을 추출한 후, '/'를 기준으로 parameter를 분리함
 
   useEffect(() => {
@@ -37,21 +36,20 @@ const PostView = () => {
         <div className="container-read">
           <h2 className="chat-detail-title">{postData.title}</h2>
           <div className="chat-detail-head">
-            <label className="chat-post-owner">작성자 : {postData.username}</label>
+            <label className="chat-post-owner">
+              작성자 : {postData.username}
+            </label>
             <label className="chat-detail-hit">조회수 : {postData.hit}</label>
           </div>
           <label className="chat-detail-content">{postData.content}</label>
           <div class="read-btns">
             <Link to="modify">
-              <a className="btn-modify">
-                수정하기
-              </a>
+              <a className="btn-modify">수정하기</a>
             </Link>
-            <a className="btn-delete">
-              삭제하기
-            </a>
+            <a className="btn-delete">삭제하기</a>
           </div>
-        </div>) : (
+        </div>
+      ) : (
         "해당 게시글을 찾을 수 없습니다."
       )}
     </div>
