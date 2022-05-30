de487ed63a2b0f647411cb46dba54f5829e2f6a1
jy05un <jys000305@naver.com>
2022-05-31 01:44:22 +0900

    취업 정보 조회 1차 샘플

---
+++ frontend/src/components/Jobs/Jobs.js
@@ -0,0 +1,135 @@
+import React from 'react'
+import './jobs.css'
+import magnify from '../../img/magnify.png';
+import FooterGray from "../Home/FooterGray";
+
+const Jobs = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-jobs'>채용중인 포지션</h1>
+                <div className='top-jobs'>
+                    <select className='select1-jobs'>
+                        <option>직군 전체</option>
+                    </select>
+                    <select className='select2-jobs'>
+                        <option>직무 전체</option>
+                    </select>
+                    <div className='search-jobs'>
+                        <button className='btn-search-jobs'>
+                            채용 관련 정보를 찾아보세요.
+                        </button>
+                        <img className='img-mag-jobs' src={magnify} alt="" />
+                    </div>
+                </div>
+                <div className='middle-jobs'>
+                    <div className='job-cards'>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                        <div className='job-card'>
+                            <div className='img-job'></div>
+                            <div className='top-job'>
+                                <div className='location-job'>대구</div>
+                                <div className='name-job'>기업명</div>
+                            </div>
+                            <div className='content-job'>구하는 개발자 포지션/정보란 (엔지니어)</div>
+                            <div className='exp-job'>경력무관</div>
+                            <div className='date-job'>채용 시 마감</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <FooterGray />
+        </div>
+    )
+}
+
+export default Jobs
