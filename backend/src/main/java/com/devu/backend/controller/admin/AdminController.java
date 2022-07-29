f8e33f1772bfa3b1c889898567ae4b90903443ac
hcs4125 <hcs4125@gmail.com>
2022-07-29 20:31:03 +0900

    feat:어드민 컨트롤러 및 어드민 홈 테스트 메서드 추가

---
+++ backend/src/main/java/com/devu/backend/controller/admin/AdminController.java
@@ -0,0 +1,20 @@
+package com.devu.backend.controller.admin;
+
+import com.devu.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController("/admin")
+public class AdminController {
+    private final UserService userService;
+
+    @GetMapping("/")
+    private ResponseEntity adminHome() {
+        return ResponseEntity.ok().body("어드민 홈 테스트");
+    }
+}
